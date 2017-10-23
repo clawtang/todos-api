a5b00f414b922b4982d7ff32ef60044e661ae0e5
clawtang <clawtang@gmail.com>
2017-10-22 22:44:28 -0700

    Complete basic API

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -67,6 +67,7 @@ RSpec.configure do |config|
   # add 'FactoryGirl' methods
   config.include FactoryGirl::Syntax::Methods
 
+  config.include RequestSpecHelper, type: :request
   # start by truncating all the tables but then use the faster trancsaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
@@ -79,5 +80,5 @@ RSpec.configure do |config|
       example.run
     end
   end
-  
+
 end
